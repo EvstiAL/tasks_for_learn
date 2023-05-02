90eea3e0baa019d57283bdf5c1b7ac208dbbaa18
EvstiAL <realtok05@yandex.ru>
2023-05-02 18:14:40 +0300

    Update script.js

--- MDK 04.02/task2/assets/js/script.js
+++ MDK 04.02/task2/assets/js/script.js
@@ -4,24 +4,37 @@ const userteleField = document.querySelector('#user-phone');
 const userMailField = document.querySelector('#user-email');
 const btnsendField = document.querySelector('#helpbtn');
 
-// userNameField.value - можно получить значение, написанное в текстовом поле
-// agreement.checked - можно узнать, нажат ли чекбокс или нет
-// если возвращает true - нажат, если false - не нажат
-
-
 btnsendField.addEventListener('click',()=>{
-    console.log('Имя пользователя:', userNameField.value);
-    console.log('Телефон пользователя:', userteleField.value);
-    console.log('Поста пользователя:', userMailField.value);
+    if (document.getElementsByClassName('help__form').value  == ""){
+        console.log('Пусто');
+    }else {
+
+        console.log('Имя пользователя:', userNameField.value);
+        console.log('Телефон пользователя:', userteleField.value);
+        console.log('Поста пользователя:', userMailField.value);
+    }
     if(agreement.checked) {
         console.log('True');
         console.log('Пользователь согласен с условиями чего-то там ');
     } else {
         console.log('False')
     }
-    
+
 })
 
+
+
+
+
+
+
+
+
+
+
+// userNameField.value - можно получить значение, написанное в текстовом поле
+// agreement.checked - можно узнать, нажат ли чекбокс или нет
+// если возвращает true - нажат, если false - не нажат
 //agreementField.addEventListener('click',()=>{
 //    console.log('Пользователь согласен с условиями чего-то там ');
 //})
