254bbb6dfd05a2520b5b95bcb3607c912d6bf018
EvstiAL <118687223+EvstiAL@users.noreply.github.com>
2023-05-03 12:36:06 +0300

    Update script.js

--- MDK 04.02/task2/assets/js/script.js
+++ MDK 04.02/task2/assets/js/script.js
@@ -4,37 +4,23 @@ const userteleField = document.querySelector('#user-phone');
 const userMailField = document.querySelector('#user-email');
 const btnsendField = document.querySelector('#helpbtn');
 
-// userNameField.value - можно получить значение, написанное в текстовом поле
-// agreement.checked - можно узнать, нажат ли чекбокс или нет
-// если возвращает true - нажат, если false - не нажат
-
-
 btnsendField.addEventListener('click',()=>{
-    console.log('Имя пользователя:', userNameField.value);
-    console.log('Телефон пользователя:', userteleField.value);
-    console.log('Поста пользователя:', userMailField.value);
+        console.log('Имя пользователя:', userNameField.value);
+        console.log('Телефон пользователя:', userteleField.value);
+        console.log('Поста пользователя:', userMailField.value);
+
+
+    if(agreement.checked) {
+        console.log('True');
+        console.log('Пользователь согласен с условиями чего-то там ');
+    } else {
+        console.log('False')
+    }
+    
 })
 
-agreementField.addEventListener('click',()=>{
-    console.log('Пользователь согласен с условиями чего-то там ');
-})
 
-// Задание: По нажатию на кнопку "Отправить" выводить в консоль данные,
-// введенные в текстовые поля, в следующем формате:
 
-// Имя пользователя: Егор
-// Телефон пользователя: +7 (999) 123-43-65
-// Почта пользователя: example@mail.ru
 
-// За вывод данных в консоль отвечает команда console.log(*какой-то текст*);
 
-// Пример:
-// const groupName = '2-ИС';
-// console.log('Название группы: ', groupName);
-// В консоль браузера выведется следующее: Название группы: 2-ИС
 
-// Дополнительно:
-// 1. Не выводить в консоль ничего, если какое-то из полей - пустое
-// 2. Также выводить "Пользователь согласен с условиями обработки",
-//    если чекбокс нажат, или "Пользователь не принимал соглашение
-//    об обработке данных", если чекбокс не нажат
