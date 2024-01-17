dd9ce1098b6b8f1267358510bb6c36e4013811f4
EvstiAl <118687223+EvstiAL@users.noreply.github.com>
2024-01-17 12:09:49 +0300

    1

--- MDK 01.03/js/script.js
+++ MDK 01.03/js/script.js
@@ -1,4 +1,34 @@
+const list = document.querySelector('.list');
+const img = document.querySelector('img');
+const users = document.querySelector('user');
 const qetPosts = async() => {
-    return await fetch('https://jsonplaceholder.typicode.com/users');
+    const response = await fetch('https://jsonplaceholder.typicode.com/users');
+    return await response.json();
 }
+const arrayOfElem = [{
+    title: '123',
+    body: '5645645'},
+    {
+            title: '123',
+    body: '5645645'},
+    {
+    title: '123',
+    body: '5645645'},
+    {
+    title: '123',
+    body: '5645645'},
+];
 
+const qetItemHTML = `
+    <li class = "item">
+        <span>${item.title}</span>
+        <p>${item.body}</p>
+    </li>
+`;
+
+arrayOfElem.forEach(item => {
+    list.innerHTML += getItemHTML(item);
+});
+
+posts.filter(post => post.userId == users.id);
+getPosts();
